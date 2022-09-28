3b414b69733b7dcd078107c5d5946ebf40d08a6f
0phoff <0phoff@users.noreply.github.com>
2022-09-28 10:00:02 +0200

    Added base Plugins

---
+++ striker/plugins/log.py
@@ -0,0 +1,86 @@
+from typing import Literal, Protocol, Union, Optional
+
+from contextlib import suppress
+import logging
+from pathlib import Path
+from ..core import Plugin, hooks
+from .._engine import Engine
+
+__all__ = ['LogPlugin']
+log = logging.getLogger(__name__)
+
+
+class ParentProtocol(Protocol):
+    log_file: Optional[Union[str, Path]] = None
+    """ Path to store logging data. No logging data will be stored if this is missing. """
+
+
+class LogPlugin(Plugin, protocol=ParentProtocol):
+    """
+    This plugin enables the :class:`~rich.logging.RichHandler` if it is installed and can also setup a logging file to store your run logs.
+
+    Args:
+        logger_name: Name of the logger for which we want to store the logs in a file. Default **root logger / all loggers**
+        file_mode: How to open the log file. Default **'a'**
+        rich_handler: Wether to enabled the :class:`~rich.logging.RichHandler` if it is available; Default **true**
+        rich_level: Filter level for the :class:`~rich.logging.RichHandler`; Default **0**
+    """
+    parent: Engine      # Fix MyPy issues by setting a proper type of self.parent
+
+    def __init__(
+        self,
+        logger_name: str = '',
+        file_mode: Literal['a', 'w'] = 'a',
+        rich_handler: bool = True,
+        rich_level: int = logging.NOTSET,
+    ):
+        self.logger = logging.getLogger(logger_name)
+        self.mode = file_mode
+        self.rich = rich_handler
+        self.level = rich_level
+
+    @hooks.engine_start
+    def setup_logging(self) -> None:
+        self.setup_streamhandler()
+        self.setup_filehandler()
+
+        # Optimization: We can safely disable this plugin, as there ar no further hooks to run.
+        self.enabled = False
+
+    def setup_streamhandler(self) -> None:
+        if not self.rich:
+            return
+
+        with suppress(ImportError):
+            from rich.logging import RichHandler
+
+            logging.basicConfig(
+                force=True,
+                level=logging.NOTSET,
+                format='%(message)s',
+                datefmt='[%X]',
+                handlers=[RichHandler(
+                    level=self.level,
+                    rich_tracebacks=True,
+                    tracebacks_suppress=['striker'],
+                )],
+            )
+
+    def setup_filehandler(self) -> None:
+        log_file = getattr(self.parent, 'log_file', None)
+        if log_file is None:
+            log.warning('"log_file" is None, so logging data will not be saved.')
+            return
+
+        self.log_file = Path(log_file)
+        if not self.log_file.parent.exists():
+            log.info('log_file folder "%s" does not exist, creating now...', self.log_file.parent)
+            self.log_file.parent.mkdir(parents=True)
+
+        handler = logging.FileHandler(filename=self.log_file, mode=self.mode)
+        handler.setLevel(logging.NOTSET)
+        handler.setFormatter(logging.Formatter(
+            fmt='%(levelname)s %(asctime)s [%(filename)s:%(lineno)d] | %(message)s',
+            datefmt='%x %X',
+        ))
+        self.logger.addHandler(handler)
---
+++ striker/plugins/progress.py
@@ -0,0 +1,268 @@
+from typing import TYPE_CHECKING, Literal, Protocol, Optional
+if TYPE_CHECKING:
+    from rich.progress import Task
+
+from datetime import timedelta
+from rich.text import Text
+from rich.progress import (     # type: ignore[attr-defined]
+    Progress,
+    ProgressColumn,
+    TextColumn,
+    BarColumn,
+    MofNCompleteColumn,
+    filesize,
+)
+from ..core import Plugin, hooks
+from .._engine import Engine
+
+__all__ = ['ProgressBarPlugin']
+
+
+class ParentProtocol(Protocol):
+    max_epochs: Optional[int] = None
+    """ Maximum number of epochs. """
+
+
+class ProgressBarPlugin(Plugin, protocol=ParentProtocol):
+    """
+    This plugin shows a Rich progress bar during runs.
+
+    Note:
+        There are 4 different progress bars, that are shown depending on the run:
+            - train_epochs: This progress bar shows the number of completed training epochs
+            - train_batches: This progress bar show the number of completed training batches
+            - validation_batches: This progress bar show the number of completed validation batches
+            - test_batches: This progress bar show the number of completed test batches
+
+        You can append extra information to each of these progress bars by setting the appropriate strings on this plugin:
+        (syntax is shown as it should be used in the engine)
+            - ``self.plugins['progressbarplugin'].train_epoch_text``
+            - ``self.plugins['progressbarplugin'].train_batch_text``
+            - ``self.plugins['progressbarplugin'].validation_text``
+            - ``self.plugins['progressbarplugin'].test_text``
+
+    Note:
+        The ``max_epoch`` value is purely used for the progress bar and will not actually stop the training.
+        See :class:`~striker.plugins.quit.QuitPlugin` for a plugin that actually stops training.
+    """
+    __type_check__: Literal['none', 'log', 'raise'] = 'none'
+    parent: Engine      # Fix MyPy issues by setting a proper type of self.parent
+
+    @hooks.engine_start
+    def start_progress(self, entry: Literal['train', 'test', 'validation']) -> None:
+        self.progress = Progress(
+            TextColumn('{task.description}'),
+            BarColumn(),
+            MofNCompleteColumn(separator=' / '),
+            TimeColumn(),
+            SpeedColumn(),
+            TextColumn('{task.fields[post_text]}'),
+            auto_refresh=False,
+        )
+        self.progress.start()
+
+        if entry == 'train':
+            self.p_epoch = self.progress.add_task(
+                'Epoch',
+                completed=self.parent.epoch,
+                total=getattr(self.parent, 'max_epochs', None),
+                post_text=self.train_epoch_text,
+            )
+            self.p_batch = self.progress.add_task(
+                'Batch',
+                start=False,
+                visible=False,
+                post_text=self.train_batch_text,
+            )
+
+        if entry in ('train', 'validation'):
+            self.p_validation = self.progress.add_task(
+                'Validation',
+                start=False,
+                visible=False,
+                post_text=self.validation_text,
+            )
+
+        if entry == 'test':
+            self.p_test = self.progress.add_task(
+                'Validation',
+                start=False,
+                visible=False,
+                post_text=self.test_text,
+            )
+
+    @hooks.engine_end
+    def stop_progress(self) -> None:
+        self.progress.stop()
+        self.progress.console.clear_live()
+
+    @hooks.train_epoch_start
+    def train_epoch_start(self) -> None:
+        self.progress.reset(
+            self.p_batch,
+            visible=True,
+            total=self.parent.mixin_loop_train.num_batches,
+            post_text=self.train_batch_text,
+        )
+        self.progress.refresh()
+
+    @hooks.train_batch_end
+    def train_batch_end(self) -> None:
+        self.progress.update(
+            self.p_batch,
+            advance=1,
+            post_text=self.train_batch_text,
+            refresh=True,
+        )
+
+    @hooks.train_epoch_end
+    def train_epoch_end(self, epoch: int) -> None:
+        self.progress.update(
+            self.p_epoch,
+            completed=epoch,
+            post_text=self.train_epoch_text,
+            refresh=True,
+        )
+
+    @hooks.validation_epoch_start
+    def validation_epoch_start(self) -> None:
+        self.progress.reset(
+            self.p_validation,
+            visible=True,
+            total=self.parent.mixin_loop_validation.num_batches,
+            post_text=self.validation_text,
+        )
+        self.progress.refresh()
+
+    @hooks.validation_batch_end
+    def validation_batch_end(self) -> None:
+        self.progress.update(
+            self.p_validation,
+            advance=1,
+            post_text=self.validation_text,
+            refresh=True,
+        )
+
+    @hooks.validation_epoch_end
+    def validation_epoch_end(self) -> None:
+        self.progress.update(self.p_validation, visible=False, refresh=True)
+
+    @hooks.test_epoch_start
+    def test_epoch_start(self) -> None:
+        self.progress.reset(
+            self.p_test,
+            visible=True,
+            total=self.parent.mixin_loop_test.num_batches,
+            post_text=self.test_text,
+        )
+        self.progress.refresh()
+
+    @hooks.test_batch_end
+    def test_batch_end(self) -> None:
+        self.progress.update(
+            self.p_test,
+            advance=1,
+            post_text=self.test_text,
+            refresh=True,
+        )
+
+    @hooks.test_epoch_end
+    def test_epoch_end(self) -> None:
+        self.progress.update(self.p_test, visible=False, refresh=True)
+
+    @property
+    def train_epoch_text(self) -> str:
+        text = getattr(self, '_train_epoch_text', None)
+        return f'\[{text}]' if text is not None else ''     # NOQA: W605 - rich requires escape of [
+
+    @train_epoch_text.setter
+    def train_epoch_text(self, value: str) -> None:
+        self._train_epoch_text = value
+
+    @property
+    def train_batch_text(self) -> str:
+        text = getattr(self, '_train_batch_text', None)
+        return f'\[{text}]' if text is not None else ''     # NOQA: W605 - rich requires escape of [
+
+    @train_batch_text.setter
+    def train_batch_text(self, value: str) -> None:
+        self._train_batch_text = value
+
+    @property
+    def validation_text(self) -> str:
+        text = getattr(self, '_validation_text', None)
+        return f'\[{text}]' if text is not None else ''     # NOQA: W605 - rich requires escape of [
+
+    @validation_text.setter
+    def validation_text(self, value: str) -> None:
+        self._validation_text = value
+
+    @property
+    def test_text(self) -> str:
+        text = getattr(self, '_test_text', None)
+        return f'\[{text}]' if text is not None else ''     # NOQA: W605 - rich requires escape of [
+
+    @test_text.setter
+    def test_text(self, value: str) -> None:
+        self._test_text = value
+
+
+class SpeedColumn(ProgressColumn):
+    """ Taken from rich.progress.TaskProgressColumn """
+    @classmethod
+    def render_speed(cls, speed: Optional[float]) -> Text:
+        if speed is None:
+            return Text('', style='progress.remaining')
+
+        if speed < 1:
+            unit, suffix = filesize.pick_unit_and_suffix(
+                int(1 / speed),
+                ['', '×10³', '×10⁶', '×10⁹', '×10¹²'],
+                1000,
+            )
+            data_speed = 1 / (speed * unit)
+            return Text(f'({data_speed:.1f}{suffix} s/it)', style='progress.remaining')
+        else:
+            unit, suffix = filesize.pick_unit_and_suffix(
+                int(speed),
+                ['', '×10³', '×10⁶', '×10⁹', '×10¹²'],
+                1000,
+            )
+            data_speed = speed / unit
+            return Text(f'({data_speed:.1f}{suffix} it/s)', style='progress.remaining')
+
+    def render(self, task: 'Task') -> Text:
+        return self.render_speed(task.finished_speed or task.speed)
+
+
+class TimeColumn(ProgressColumn):
+    """ Taken from rich.progress.TimeRemainingColumn and rich.progress.TimeElapsedColumn """
+    def render(self, task: 'Task') -> Text:
+        if task.finished:
+            return self.render_elapsed(task, 'progress.elapsed')
+        else:
+            return Text.assemble(
+                self.render_elapsed(task, 'progress.elapsed'),
+                Text(' / ', style='progress.elapsed'),
+                self.render_remaining(task, 'progress.elapsed'),
+            )
+
+    def render_elapsed(self, task: 'Task', style: str) -> Text:
+        elapsed = task.finished_time if task.finished else task.elapsed
+        if elapsed is None:
+            return Text('--:--:--', style=style)
+
+        delta = timedelta(seconds=int(elapsed))
+        return Text(str(delta), style=style)
+
+    def render_remaining(self, task: 'Task', style: str) -> Text:
+        if task.total is None:
+            return Text('', style=style)
+        if task.time_remaining is None:
+            return Text('--:--:--', style=style)
+
+        minutes, seconds = divmod(int(task.time_remaining), 60)
+        hours, minutes = divmod(minutes, 60)
+        formatted = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+
+        return Text(formatted, style=style)
