7691c917dabe21463e77ee8e8393aae458b16140
0phoff <0phoff@users.noreply.github.com>
2022-09-28 14:56:05 +0200

    Added dataloader to the post() method

--- striker/mixins/loop_test.py
+++ striker/mixins/loop_test.py
@@ -18,12 +18,13 @@ class ParentProtocol(Protocol):
             output that gets aggregated in a list for :meth:`~ParentProtocol.post()``.
         """
 
-    def post(self, output: list[Any]) -> Any:
+    def post(self, output: list[Any], dataloader: DataLoader[Any]) -> Any:
         """
         Post-processing of the network output.
 
         Args:
-            data: Aggregated output from :meth:`~ParentProtocol.infer()`.
+            output: Aggregated output from :meth:`~ParentProtocol.infer()`.
+            dataloader: Dataloader that was used to run the model.
 
         Returns:
             Post-processed output such as metrics, losses, etc.
@@ -139,6 +140,6 @@ class TestLoopMixin(LoopMixin, protocol=ParentProtocol):
             self.parent.run_hook(type=f'{self.name}_batch_end', index=batch, args=(batch, outputs[-1]))
             yield
 
-        output = self.parent.post(outputs)
+        output = self.parent.post(outputs, dataloader)
 
         self.parent.run_hook(type=f'{self.name}_epoch_end', args=(output,))
