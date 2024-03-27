06126a3004826b92479cd3f6ef1011f52d1b20bb
0phoff <0phoff@users.noreply.github.com>
2024-03-27 12:57:57 +0100

    Fix import order in root __init__.py
    It is important that CLI import happens last

--- striker/__init__.py
+++ striker/__init__.py
@@ -1,7 +1,7 @@
-from ._cli import CLI
-from ._engine import Engine
+from ._engine import Engine  # NOQA: I001 - CLI import needs to happen last
 from ._parameter import Parameters
 from .core import hooks
+from ._cli import CLI
 
 __all__ = ['Engine', 'Parameters', 'CLI', 'hooks']
 __version__ = '0.0.0'
